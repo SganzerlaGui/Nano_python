376df6b9dc6f37c37a0db943a86df03447299ea9
SganzerlaGui <gui.sganzerla2007@gmail.com>
2026-04-01 10:33:23 -0300

    versões novas

---
+++ arquivo.py
@@ -0,0 +1,8 @@
+def perguntar():
+# O return:é usada para retornar um valor de uma função. Quando uma função é chamada, ela executa um bloco de código e, em seguida, retorna um valor
+    return input("Oque você deseja realizar?: \n" +
+              "<I> - para cadastrar usuario\n" +
+              "<P> - para Pesquisar usuario\n" +
+              "<E> - para Excluir usuario\n" +
+              "<L> - para Listar usuario\n" +
+              "<S> - para Sair\n").upper()
--- listas.py
+++ listas.py
@@ -1,2 +1,32 @@
+from NANO_PYTHON_FIAP.arquivo import *
+
+#usuarios = {}
+#
+#print(usuarios)
+#
+#usuarios = {
+#        "Guilherme" : ["Guilherme Sganzerla", "20/03/2026", "Programador"],
+#        "Tatiany" : ["Tatiany Sganzerla", "20/03/2026", "Fisioterapeuta"]
+#}
+#
+#print(usuarios)
+#
+#usuarios ["Florinda"] = ["Florinda da Silva", "09/04/2025", "Juiza"] 
+#
+#print(usuarios)
+
+
 usuarios = {}
+
+opcao = perguntar()
+            
+while opcao == "I" or opcao == "P" or opcao == "E" or opcao == "L" or opcao == "S":
+    
+    if opcao == "I":
+        chave = input("Digite o nome do usuario: ").upper()
+        nome = input("Digite o nome do usuario: ").upper()
+        nascimento = input("Digite a data de nascimento do usuario (Use: d/m/a): ")
+        usuarios[chave] = [nome], [nascimento]
+    
+    opcao = perguntar()
 
