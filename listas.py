712ead05988c8e9e6deab1883925e96d167a25be
Guilherme Sganzerla Correia <gui.sganzerla2007@gmail.com>
2026-04-07 20:58:53 -0300

    Update listas.py

--- listas.py
+++ listas.py
@@ -34,20 +34,11 @@ import os
 #---Lista = []
 #---dicionnario {}
 #---tupla()    
-
-
-usuarios = {}
-
-emails = ["Gsc@gmail.com",]
-
-tupla = list(enumerate(emails))
-
 #------Tupla para retornar mais de 1 valor de 1 metodo
 #--------enumerate vai voltar enumerado: 11,2,3,4,5,6,7......
 #l-------list concatena a numeração e os gmails em uma tupla
 
-for chave in range(0, len(tupla)):
-    
+for chave in range(0, len(tupla)):    
     #print(f"Email:", tupla[chave][1])
     novo_email = input("Digite o seu email: ")
     usuario = input("Digite o seu usuario: ")
@@ -77,3 +68,56 @@ print(emails)
 # ---------[0] -> Pega a primeira informação da lista (o Nome)
 
 # --------[1] -> Pega a segunda informação da lista (o Nível)
+
+
+
+
+from captura import *
+
+usuarios = {}
+
+def menu():
+    print("1 - Inserir usuario\n" +
+          "2 - Pesquisar usuario\n" +
+          "3 - Excluir usuario\n" +
+          "4 - Listar usuarios\n" +
+          "5 - Sair\n")
+    
+    # Chamando a função que está no captura.py
+    return pegar_opcao()
+
+# --- Execução (que volta ao menu após concuido)---
+
+while True:
+    clean()
+    opcao = menu()
+
+    if opcao == "1":
+        inserir(usuarios)
+        print(usuarios)
+    
+    elif opcao == "2":
+        pesquisar(usuarios)
+    
+    elif opcao == "3":
+        excluir(usuarios)
+    
+    elif opcao == "4":
+        listar(usuarios)
+    
+    elif opcao == "5":
+        sair()
+        #break # Quebra o loop e fecha o programa
+    
+    else:
+        print("Opção inválida!")
+
+    # Pausa para o usuário conseguir ler o que aconteceu antes de limpar a tela
+    input("\nPressione ENTER para voltar ao menu...")
+    break
+
+clean()
+
+nome_da_pessoa = inserir(usuarios) 
+
+print(f"Acabamos de cadastrar o {nome_da_pessoa}!")
